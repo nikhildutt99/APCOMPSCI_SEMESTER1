79d76cbc5c0b5fc4d7d2aea0fd19d7c032dce071
nikhildutt99 <DuttN5032@sduhsd.lan>
2016-09-02 14:02:39 -0700

    ascii art 2

---
+++ asciiart2.java
@@ -0,0 +1,26 @@
+public class asciiart2
+{
+
+		public static void main(String[]args)
+		{
+		    System.out.println("    -");
+			System.out.println("    -");
+			System.out.println("    -");
+			System.out.println("    -");
+			System.out.println("    -");
+			System.out.println("    -");
+			System.out.println("    -");
+			System.out.println("    -");
+			System.out.println("    -");
+			System.out.println(".......");
+			System.out.println("  ---  ");
+			System.out.println("  ---  ");
+			System.out.println("  ---  ");
+			System.out.println("  ---  ");
+			
+		
+	
+			
+		}
+		
+}
