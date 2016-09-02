31f070d4206bff1b5ebd7f0ea4e182c37e88c155
nikhildutt99 <DuttN5032@sduhsd.lan>
2016-09-02 13:32:38 -0700

    ascii ART 1

---
+++ ascii ART 1.java
@@ -0,0 +1,14 @@
+public class firstprog
+{
+
+		public static void main(String[]args)
+		{
+		    System.out.println("@  @");
+			System.out.println("    I");
+			System.out.println("........");
+		
+	
+			
+		}
+		
+}
