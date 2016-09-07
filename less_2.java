d0f99b10963a1851b6b435d021c71b3158932d0b
nikhildutt99 <DuttN5032@sduhsd.lan>
2016-09-07 13:31:40 -0700

    lesson 2

---
+++ less_2.java
@@ -0,0 +1,19 @@
+public class less_2
+{
+	public static void main(String[]args)
+	{
+		int x = 2;
+		int y = 4;
+		String Name = "Nikhil";
+		String address = "Manor Ridge Lane";
+		String city = "San Diego";
+		String zip = "92130";
+		int length = 7;
+		int width = 7;
+		int height = 7;
+		System.out.println("The surface area of a cube with edge length 7 is " + length*width*6);
+		System.out.println( Name + " lives in " + address + " , " + city + " , " + zip);
+		System.out.println(" 2 times 4 is " + x * y);
+	
+	}
+}
