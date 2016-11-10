3b839b94b07d5f6267dfc6ad23e893c4b7c3a42e
nikhildutt99 <nikhildutt99@gmail.com>
2016-11-10 13:35:23 -0800

    comp

---
+++ Lesson 7/Lesson7_Ex1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Lesson7_Ex1
+{
+	public static void main(String[]args)
+	{
+		Scanner kb = new Scanner(System.in);
+		System.out.println("Please enter a number:");
+		int num = kb.nextInt();
+		int sum = 0;
+		sumAdder(num,sum);
+	}
+	public static void sumAdder(int num, int sum)
+	{
+		while(num>0){
+			sum = sum + (num % 10);
+			num = num / 10;
+		}
+		System.out.println("The sum of your digits is " + sum);
+	}
+}
